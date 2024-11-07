e63c0924791dc55ff4fdb6dc7cabd4449299c0d2
aabidh-op <142312703+aabidh-op@users.noreply.github.com>
2024-11-07 14:22:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,8 +11,8 @@ resource "aws_security_group" "db" {
   }
 
   egress {
-    from_port        = var.from_port
-    to_port          = var.to_port
+    from_port        = 0
+    to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
